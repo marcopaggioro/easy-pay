a1d3016597df29264a02567b93d8cb7ec6cad0a3
Marco Paggioro <marco.paggioro@gmail.com>
2025-04-15 22:18:36 +0200

    Simpler init users

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -5,4 +5,4 @@ curl "http://localhost:9000/user" -H "Content-Type: application/json" -H "Origin
     --data-raw "{\"firstName\":\"Mario\",\"lastName\":\"Rossi\",\"birthDate\":\"1990-10-27\",\"email\":\"mario@rossi.it\",\"encryptedPassword\":\"2bbe0c48b91a7d1b8a6753a8b9cbe1db16b84379f3f91fe115621284df7a48f1cd71e9beb90ea614c7bd924250aa9e446a866725e685a65df5d139a5cd180dc9\"}"
 
 curl "http://localhost:9000/user" -H "Content-Type: application/json" -H "Origin: http://localhost:4200" \
-    --data-raw "{\"firstName\":\"Giovanni\",\"lastName\":\"Pirozzi\",\"birthDate\":\"1996-05-15\",\"email\":\"giovanni@pirozzi.it\",\"encryptedPassword\":\"2bbe0c48b91a7d1b8a6753a8b9cbe1db16b84379f3f91fe115621284df7a48f1cd71e9beb90ea614c7bd924250aa9e446a866725e685a65df5d139a5cd180dc9\"}"
+    --data-raw "{\"firstName\":\"Luca\",\"lastName\":\"Moro\",\"birthDate\":\"1996-05-15\",\"email\":\"luca@moro.it\",\"encryptedPassword\":\"2bbe0c48b91a7d1b8a6753a8b9cbe1db16b84379f3f91fe115621284df7a48f1cd71e9beb90ea614c7bd924250aa9e446a866725e685a65df5d139a5cd180dc9\"}"

627be45be50c9375d61104cc632eaf5ea7b7944e
Marco Paggioro <marco.paggioro@gmail.com>
2025-04-17 18:34:22 +0200

    better windows start script

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,2 +1,2 @@
-docker run --rm -v %cd%/backend:/root sbtscala/scala-sbt:eclipse-temurin-23.0.2_7_1.10.11_3.3.5 sbt clean compile Docker/stage
+docker run --rm -v .\backend:/root sbtscala/scala-sbt:eclipse-temurin-23.0.2_7_1.10.11_3.3.5 sbt clean compile Docker/stage
 docker compose up --detach --build

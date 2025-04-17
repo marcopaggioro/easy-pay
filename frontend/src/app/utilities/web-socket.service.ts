ece2e1dba0966eb8d14ad8647c45d74910655a54
Marco Paggioro <marco.paggioro@gmail.com>
2025-04-17 16:12:30 +0200

    FE: removed another TODO

--- frontend/src/app/utilities/web-socket.service.ts
+++ frontend/src/app/utilities/web-socket.service.ts
@@ -62,10 +62,10 @@ export class WebSocketService {
     }
   }
 
-  //TODO perchè due connection al load della pagina
   //TODO non ricrea la connection
   getWebSocketMessages(): Observable<WebSocketMessage | null> {
-    if (!this.socket$ || !this.socket$.closed) {
+    if (!this.socket$ || this.socket$.closed) {
+      console.log("creating")
       this.createWebSocketConnection();
     }
     return this.messageSubject$.asObservable();

51584b132091216033c88038d51833e8c2d6e16e
Marco Paggioro <marco.paggioro@gmail.com>
2025-04-20 11:06:41 +0200

    FE: fix validation in profile editing

--- frontend/src/app/dashboard/profile/profile-field/profile-field.component.ts
+++ frontend/src/app/dashboard/profile/profile-field/profile-field.component.ts
@@ -1,4 +1,14 @@
-import {Component, ElementRef, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild} from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 import {NgIf} from '@angular/common';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {APP_CONSTANTS} from '../../../app.constants';
@@ -12,7 +22,7 @@ import {FormControl, ReactiveFormsModule, ValidatorFn, Validators} from '@angula
   ],
   templateUrl: './profile-field.component.html'
 })
-export class ProfileFieldComponent implements OnChanges {
+export class ProfileFieldComponent implements OnInit, OnChanges {
   @ViewChild('inputField') set inputFieldRef(input: ElementRef | null) {
     if (input && this.editing) {
       input.nativeElement.focus();
@@ -26,15 +36,19 @@ export class ProfileFieldComponent implements OnChanges {
   @Input() inputType!: string;
   @Input() additionalValidators: ValidatorFn[] = [];
   @Input() fieldTransformation?: (input: string) => string;
-
   @Output() editResult = new EventEmitter<string | null>();
 
   protected editing = false;
   protected waitingResponse = false;
 
-  protected formField = new FormControl<string>('', [Validators.required, ...this.additionalValidators]);
+  protected formField = new FormControl<string | null>(null);
 
   constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    // Fields annotated with @Input() are not reactive, so we need to manually set the value
+    this.formField = new FormControl<string>(this.fieldValue || '', [Validators.required, ...this.additionalValidators]);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -48,34 +62,41 @@ export class ProfileFieldComponent implements OnChanges {
   }
 
   editField(): void {
-    if (this.fieldValue != this.formField.value) {
-      if (!this.waitingResponse) {
-        this.waitingResponse = true;
-
-        const body = {
-          [this.httpFieldName]: this.fieldTransformation ? this.fieldTransformation(this.formField.value!) : this.formField.value!
-        };
-
-        this.http.patch(APP_CONSTANTS.ENDPOINT_USER_UPDATE, body, {
-          withCredentials: true,
-          responseType: 'json'
-        }).subscribe({
-          next: () => {
-            this.editResult.emit(null);
-            this.editing = false;
-            this.waitingResponse = false;
-            this.formField.setValue(this.fieldValue || '');
-          },
-          error: (httpErrorResponse: HttpErrorResponse) => {
-            this.editResult.emit(httpErrorResponse?.error?.error || APP_CONSTANTS.MESSAGE_GENERIC_ERROR);
-            this.editing = false;
-            this.waitingResponse = false;
-            this.formField.setValue(this.fieldValue || '');
-          }
-        });
-      }
-    } else {
+    if (this.formField.invalid) {
+      console.warn("Trying to submit with invalid data")
       this.editing = false;
+      return;
+    }
+
+    if (this.fieldValue != this.formField.value) {
+      this.editing = false;
+      return;
+    }
+
+    if (!this.waitingResponse) {
+      this.waitingResponse = true;
+
+      const body = {
+        [this.httpFieldName]: this.fieldTransformation ? this.fieldTransformation(this.formField.value!) : this.formField.value!
+      };
+
+      this.http.patch(APP_CONSTANTS.ENDPOINT_USER_UPDATE, body, {
+        withCredentials: true,
+        responseType: 'json'
+      }).subscribe({
+        next: () => {
+          this.editResult.emit(null);
+          this.editing = false;
+          this.waitingResponse = false;
+          this.formField.setValue(this.fieldValue || '');
+        },
+        error: (httpErrorResponse: HttpErrorResponse) => {
+          this.editResult.emit(httpErrorResponse?.error?.error || APP_CONSTANTS.MESSAGE_GENERIC_ERROR);
+          this.editing = false;
+          this.waitingResponse = false;
+          this.formField.setValue(this.fieldValue || '');
+        }
+      });
     }
   }
 
